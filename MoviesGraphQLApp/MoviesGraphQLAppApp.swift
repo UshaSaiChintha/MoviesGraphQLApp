12af6a28571a6dfbf716ff12000c89f19700100d
Usha Sai Chintha <ushasai154@gmail.com>
2023-06-13 13:43:42 +0530

    Initial Commit

---
+++ MoviesGraphQLApp/MoviesGraphQLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesGraphQLAppApp.swift
+//  MoviesGraphQLApp
+//
+//  Created by Usha Sai Chintha on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesGraphQLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
